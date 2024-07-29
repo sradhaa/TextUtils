a982df8f9f45fc318265a6a534847bc5cfa0e3e2
sradhanjali behera <sradhaa@users.noreply.github.com>
2024-07-29 16:32:59 +0530

    Update Navbar.js

--- src/componenets/Navbar.js
+++ src/componenets/Navbar.js
@@ -11,7 +11,7 @@ export default function Navbar(props) {
       className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
     >
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
+        <a className="navbar-brand" href="#">
           {props.title}
         </a>
         <button
